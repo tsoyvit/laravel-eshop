da39a7fdbb923b9c54ee88c2f6ec4833954fffb8
Vitaliy <tsoyvetal@gmail.com>
2025-06-23 17:30:46 +0300

    +sonar, ci/cd

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,9 +13,7 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         $this->seed();
-
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
